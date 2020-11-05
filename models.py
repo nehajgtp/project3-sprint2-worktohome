7df1f2233ffda3016ff1ef20900bd7519e52b303
Matthew-J-M <mjm227@njit.edu>
2020-11-05 00:11:09 +0000

    table name is the user's email address. Created a class called searchObject. -> Contains address, the primary key for each record -> Contains price__low, which is the low end of the pricing -> Contains price__high, which is the high end of the pricing. -> Contains distence, which is the distence specified from the address address has be increased from 120 to 255 characters. Added listings which is an db.Array Added global array to keep track of all the listings

--- models.py
+++ models.py
@@ -2,14 +2,32 @@
 import flask_sqlalchemy
 from app import db
 
+LISTINGS = {}
 
-class Usps(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    address = db.Column(db.String(120))
+class table_defintion(db.Model):
+    address = db.Column(db.String(255), primary_key=True)
+    listings = db.Column(db.Array)
     
-    def __init__(self, a):
-        self.address = a
+    def __init__(self, string, price_one, price_two, dist, new_listing):
+        search_data = search_parameters(string, price_one, price_two, dist)
+        self.address = search_data.address
+        LISTINGS.get(search_data.address, new_listing)
+        listings = LISTINGS
         
     def __repr__(self):
-        return '<Usps address: %s>' % self.address 
+        return '<The address: %s>' % self.address 
 
+class search_parameters():
+    address = ""
+    price_range_low = 0
+    price_range_high = 0
+    distance = 0
+    
+    def __init__(self, string, price_one, price_two, dist):
+        self.address = string;
+        self.price_range_low = price_one;
+        self.price_range_low = price_two;
+        self.distance = dist
+        
+    def __repr__(self):
+        return '<The address is %s, the price is between %d and %d, and the distance is %d.>' % self.address %self.price_range_low %self.price_range_high %self.distance
