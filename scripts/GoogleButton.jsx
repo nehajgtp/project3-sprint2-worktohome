6f25cb0cc267a6a86dab6c0e272129be50dc5e41
kevinng250 <kevinng250@gmail.com>
2020-11-06 23:13:06 +0000

    Removed onFailure and unneeded code from GoogleButton.jsx

--- scripts/GoogleButton.jsx
+++ scripts/GoogleButton.jsx
@@ -17,28 +17,7 @@ export function GoogleButton(){
             clientId="1034127712778-v6qvk1ma6ilbg141bvuitipumnvklo4j.apps.googleusercontent.com"
             buttonText ="Login"
             onSuccess={handleSubmit}
-            onFailure={handleSubmit}
             cookiePolicy="single_host_origin"
         />
     );
 }
-
-// const GoogleButton = () => {
-//     const history = useHistory();
-
-//     const handleSubmit = () => {
-//         history.push("/content");
-//     }
-
-//     return (
-//         <GoogleLogin
-//             clientId="1034127712778-v6qvk1ma6ilbg141bvuitipumnvklo4j.apps.googleusercontent.com"
-//             buttonText ="Login"
-//             onSuccess={handleSubmit}
-//             onFailure={handleSubmit}
-//             cookiePolicy="single_host_origin"
-//         />
-//     );
-// }
-
-// export default GoogleButton;
