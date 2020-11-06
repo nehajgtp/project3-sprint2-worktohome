156a4792fde08bcf24feb6089f85937ad9f49430
Neha Jagtap <ec2-user@ip-172-31-24-19.ec2.internal>
2020-11-06 00:47:38 +0000

    Add test socket to send listings when search is made, replace socket variables

--- scripts/SearchListings.jsx
+++ scripts/SearchListings.jsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 
+import { Socket } from './Socket';
+
+
 export function SearchListings() {
     var dict = [{
         address: "1600 Pennsylvania Ave, Washington, DC",
@@ -9,7 +12,15 @@ export function SearchListings() {
         address: "1600 Pennsylvania Ave, Washington, DC",
         image: "https://d3g9pb5nvr3u7.cloudfront.net/sites/54e605552720c85d64735cc5/-911839127/256.png"
     }];
-    const [listings, setListings] = React.useState(dict)
+    const [listings, setListings] = React.useState(dict);
+    
+    function onSearch() {
+        Socket.on('receive listings', (data) => {
+            setListings(data['listings']);
+        });
+    }
+    
+    onSearch();
     
     return (
         <div>
