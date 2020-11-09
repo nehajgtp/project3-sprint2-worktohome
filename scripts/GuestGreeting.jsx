15d79d9710b8cec7423089a4e14a3fe435c10e1a
Neha Jagtap <ec2-user@ip-172-31-24-19.ec2.internal>
2020-11-09 22:04:01 +0000

    Lint files

--- scripts/GuestGreeting.jsx
+++ scripts/GuestGreeting.jsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { GoogleButton } from './GoogleButton';
-export function GuestGreeting() {
-    return (
-        <div>  
-            <h2> Work to Home </h2>
-            <p>Make your home search more convenient to your commute!</p>
-            <GoogleButton/>
-        </div>
-        );
+import GoogleButton from './GoogleButton';
+
+export default function GuestGreeting() {
+  return (
+    <div>
+      <h2> Work to Home </h2>
+      <p>Make your home search more convenient to your commute!</p>
+      <GoogleButton />
+    </div>
+  );
 }
--- scripts/Main.jsx
+++ scripts/Main.jsx
@@ -1,26 +1,23 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Content from './Content';
-import { GuestGreeting } from './GuestGreeting'
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import GuestGreeting from './GuestGreeting';
 
-
-function Greeting(){
-    return (
-        <div>
-            <Router>
-                <Switch>
-                <Route exact path="/" component={GuestGreeting}/>
-                <Route path="/content" component={Content}/>
-                </Switch>
-            </Router>
-        </div>
-    );
+function Greeting() {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={GuestGreeting} />
+          <Route path="/content" component={Content} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
-
 ReactDOM.render(
-    <Greeting/>, 
-    document.getElementById('content')
-    );
+  <Greeting />,
+  document.getElementById('content'),
+);
