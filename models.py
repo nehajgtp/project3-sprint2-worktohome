83712ef3d3da0a9bd42e5f564a2a775d0338963c
Matthew-J-M <mjm227@njit.edu>
2020-11-09 22:01:35 +0000

    Changed table makeup to display -> Unique ID for each search, email as a string, address as a string, price_low as a intger, price_high as an integer, distance as an integer

--- models.py
+++ models.py
@@ -2,18 +2,22 @@
 import flask_sqlalchemy
 from app import DB
 
-LISTINGS = {}
-
 class table_defintion(DB.Model):
-    address = DB.Column(DB.String(255), primary_key=True)
-    listings = DB.Column(DB.Array)
-    
-    def __init__(self, string, price_one, price_two, dist, new_listing):
-        search_data = search_parameters(string, price_one, price_two, dist)
+    id = DB.Column(DB.Integer, primary_key=True)   
+    email = DB.Column(DB.String(255))
+    address = DB.Column(DB.String(255))
+    price_low = DB.Column(DB.Integer)
+    price_high = DB.Column(DB.Integer)
+    distance = DB.Column(DB.Integer)
+
+    def __init__(self, email, address, price_one, price_two, dist, new_listing):
+        search_data = search_parameters(address, price_one, price_two, dist)
+        self.email = email
         self.address = search_data.address
-        LISTINGS.get(search_data.address, new_listing)
-        listings = LISTINGS
-        
+        self.price_low = search_data.price_range_low
+        self.price_high = search_data.price_range_high
+        self.distance = search_data.distance
+
     def __repr__(self):
         return '<The address: %s>' % self.address 
 
