51f62f3c0fb7a4643747954a0462d4ad41a750a7
Kevin Ng <kevinng250@gmail.com>
2020-11-21 03:04:47 +0000

    Add No Listing Message

--- scripts/SearchListings.jsx
+++ scripts/SearchListings.jsx
@@ -1,39 +1,73 @@
 import * as React from 'react';
-
+import { useEffect } from 'react';
 import { Socket } from './Socket';
 
 export default function SearchListings() {
   const [listings, setListings] = React.useState([]);
+  const [result, setResult] = React.useState("");
 
   function onSearch() {
     Socket.on('sending listing', (data) => {
+      console.log(data)
       setListings(data);
+      if(data.length === 0){
+        setResult(<h2>"No Listings Found"</h2>)
+      }
+      else{
+        setResult((
+          <div>{
+            listings.map(
+              (listing) => (
+                <p>
+                  Address: {listing.home_street}, {listing.home_city}, {listing.home_state_code}
+                  <br />
+                  <img id="house" alt="" src={listing.home_image} />
+                  <br />
+                  Price: ${listing.home_price}
+                  <br />
+                  Beds: {listing.home_baths}
+                  <br />
+                  Baths: {listing.home_beds}
+                  <hr />
+                </p>
+              ),
+            )
+          }</div>))
+        }
+      console.log(result)
     });
   }
-
+  // function results(){
+  //   console.log("hi")
+  //   if(listings === [] ){
+  //     return (<span>No Listings found</span>)
+  //   }
+  //   return (
+  //     <div>{
+  //       listings.map(
+  //         (listing) => (
+  //           <p>
+  //             Address: {listing.home_street}, {listing.home_city}, {listing.home_state_code}
+  //             <br />
+  //             <img id="house" alt="" src={listing.home_image} />
+  //             <br />
+  //             Price: ${listing.home_price}
+  //             <br />
+  //             Beds: {listing.home_baths}
+  //             <br />
+  //             Baths: {listing.home_beds}
+  //             <hr />
+  //           </p>
+  //         ),
+  //       )
+  //   }</div>)
+  // }
   onSearch();
-
+  // results();
   return (
     <div>
       <h2>Listings</h2>
-      {
-                listings.map(
-                  (listing) => (
-                    <p>
-                      Address: {listing.home_street}, {listing.home_city}, {listing.home_state_code}
-                      <br />
-                      <img id="house" alt="" src={listing.home_image} />
-                      <br />
-                      Price: ${listing.home_price}
-                      <br />
-                      Beds: {listing.home_baths}
-                      <br />
-                      Baths: {listing.home_beds}
-                      <hr />
-                    </p>
-                  ),
-                )
-            }
+      {result}
     </div>
   );
 }
