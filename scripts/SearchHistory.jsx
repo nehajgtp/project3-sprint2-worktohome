cdec8506d644f9b297e9a3d2de3ea5659b43a619
Matthew-J-M <mjm227@njit.edu>
2020-11-23 20:13:26 +0000

    Fixed an issue where the socket would be called over and over again by locking the socket calls behind a boolean.

--- scripts/SearchHistory.jsx
+++ scripts/SearchHistory.jsx
@@ -1,41 +1,63 @@
 import * as React from 'react';
 import { Socket } from './Socket';
+import { useHistory } from 'react-router-dom';
+
 
 export default function SearchHistory(){
     const [list, changeList] = new React.useState([]);
+    const [boolean, changeBoolean] = new React.useState(false);
+    const history = useHistory();
     
+
     function goToHistory(){
+        changeBoolean(true);
+        console.log("Before waiting on socket.")
         Socket.on("received database info", (data) => {
             changeList(data);
         });
     }
-    Socket.emit("request search history", 1);
     
-    goToHistory();
-    if(list != null){
-    return <div>
-    <h1>List of all searches</h1>
-    {   
-        list.map(
-                  (listing) => (
-                    <p>
-                      Address: {listing.address}
-                      <br />
-                      <img id="house" alt="" src={listing.home_image} />
-                      <br />
-                      Low Price: ${listing.price_low}
-                      <br />
-                      High Price: {listing.price_high}
-                      <br />
-                      Distance from Address: {listing.distance}
-                      <hr />
-                    </p>
-                  ),
-                )
+    function goToSearchEngine(){
+        history.push("/content");
     }
-    </div>;
+    
+    if(boolean == false){
+        console.log("Before emit.")
+        Socket.emit("request search history", 1);
+        console.log("Before call to goToHistory")
+        goToHistory();
+    }
+    console.log("Going to display.")
+    var objects = list;
+    //console.log(objects)
+    if(objects.length != 0){
+        return <div>
+        <h1>List of all searches</h1>
+        {   
+            objects.map(
+                      (listing) => (
+                        <p>
+                          Address: {listing.address}
+                          <br />
+                          <img id="house" alt="" src={listing.home_image} />
+                          <br />
+                          Low Price: ${listing.price_low}
+                          <br />
+                          High Price: {listing.price_high}
+                          <br />
+                          Distance from Address: {listing.distance}
+                          <hr />
+                        </p>
+                      ),
+                    )
+        }
+        <button type="button" onClick={goToSearchEngine}>Go back to search page.</button>
+        </div>;
     }
     else {
-        return <h1>No searches for this user.</h1>
+        return <div>
+        <h1>No searches for this user.</h1>
+        <button type="button" onClick={goToSearchEngine}>Go back to search page.</button>
+        </div>;
     }
 }
