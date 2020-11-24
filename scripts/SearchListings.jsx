2cfcf4c937e5647828dc789c67b9ffa8bf8db943
Kevin Ng <kevinng250@gmail.com>
2020-11-24 00:48:51 +0000

    No Listings found Alert

--- scripts/SearchListings.jsx
+++ scripts/SearchListings.jsx
@@ -3,76 +3,35 @@ import { useEffect } from 'react';
 import { Socket } from './Socket';
 
 export default function SearchListings() {
-  const [listings, setListings] = React.useState([]);
-  // const [result, setResult] = React.useState("");
+  const [listings, setListings] = React.useState(false);
+  const [result, setResult] = React.useState("");
 
   function onSearch() {
     Socket.on('sending listing', (data) => {
       console.log(data)
-      setListings(data);
-      // if(data.length === 0){
-      //   setResult(<h2>"No Listings Found"</h2>)
-      // }
-      // else{
-      //   setResult((
-      //     <div>{
-      //       listings.map(
-      //         (listing) => (
-      //           <p>
-      //             Address: {listing.home_street}, {listing.home_city}, {listing.home_state_code}
-      //             <br />
-      //             <img id="house" alt="" src={listing.home_image} />
-      //             <br />
-      //             Price: ${listing.home_price}
-      //             <br />
-      //             Beds: {listing.home_baths}
-      //             <br />
-      //             Baths: {listing.home_beds}
-      //             <hr />
-      //           </p>
-      //         ),
-      //       )
-      //     }</div>))
-      //   }
-      // console.log(result)
+      if(data.length == 0){
+        setListings("None Found")
+      }
+      else{setListings(data);}
+      
     });
   }
-  // function results(){
-  //   console.log("hi")
-  //   if(listings === [] ){
-  //     return (<span>No Listings found</span>)
-  //   }
-  //   return (
-  //     <div>{
-  //       listings.map(
-  //         (listing) => (
-  //           <p>
-  //             Address: {listing.home_street}, {listing.home_city}, {listing.home_state_code}
-  //             <br />
-  //             <img id="house" alt="" src={listing.home_image} />
-  //             <br />
-  //             Price: ${listing.home_price}
-  //             <br />
-  //             Beds: {listing.home_baths}
-  //             <br />
-  //             Baths: {listing.home_beds}
-  //             <hr />
-  //           </p>
-  //         ),
-  //       )
-  //   }</div>)
-  // }
-  // useEffect(() => {
-  //   results()
-  // }, [listings]);
+  
   onSearch();
-  // results();
-  return (
-    <div>
-      <h2>Listings</h2>
-     {
-            listings.map(
-              (listing) => (
+  function results(){
+    console.log(listings)
+    if(listings === false){
+      setResult(<span>Enter an Address</span>)
+    }
+    else if(listings === "None Found"){
+      setResult(<span>No Listings Found</span>)
+    }
+    else{
+      setResult(
+        <ul>{
+          listings.map(
+            (listing) => (
+            <li key = {listing}>
                 <p>
                   Address: {listing.home_street}, {listing.home_city}, {listing.home_state_code}
                   <br />
@@ -83,11 +42,20 @@ export default function SearchListings() {
                   Beds: {listing.home_baths}
                   <br />
                   Baths: {listing.home_beds}
-                  <hr />
                 </p>
-              ),
-            )
-          }
+              </li>
+            ),
+          )
+      }</ul>)
+    }
+  }
+  useEffect(() => {
+    results()
+  }, [listings]);
+  return (
+    <div>
+      <h2>Listings</h2>
+      {result}
     </div>
   );
 }
