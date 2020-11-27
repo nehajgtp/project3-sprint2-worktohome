5da569c1fd1bada7d7e7567cbcf516e446b50504
Neha Jagtap <ec2-user@ip-172-31-20-119.ec2.internal>
2020-11-27 00:34:11 +0000

    Add about section

--- scripts/GuestGreeting.jsx
+++ scripts/GuestGreeting.jsx
@@ -5,10 +5,17 @@ import './GuestGreeting.css';
 
 export default function GuestGreeting() {
   return (
-    <div class="title">
-      <img id="logo" src="https://i.imgur.com/Zi1Oxa7.png" width="350" height="350" />
-      <p>Find the best home for your commute!</p>
-      <GoogleButton />
+    <div>
+      <div class="title">
+        <img id="logo" src="https://i.imgur.com/Zi1Oxa7.png" width="350" height="350" />
+        <p>Find the best home for your commute!</p>
+        <GoogleButton />
+      </div>
+      <div class="about">
+      <div id="about-section">
+        Our website tailors your home search to your specific commute, providing information about the commute from each listing to your desired destination.
+      </div>
+      </div>
     </div>
   );
 }
