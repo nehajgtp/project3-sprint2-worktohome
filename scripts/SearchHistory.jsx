b03373ec9b255b5a6a3917a8bf9740c656c7f99a
Matthew-J-M <mjm227@njit.edu>
2020-11-27 16:55:29 +0000

    Linted SearchHistory.jsx

--- scripts/SearchHistory.jsx
+++ scripts/SearchHistory.jsx
@@ -1,63 +1,64 @@
 import * as React from 'react';
-import { Socket } from './Socket';
 import { useHistory } from 'react-router-dom';
+import { Socket } from './Socket';
 
+export default function SearchHistory() {
+  const [List, changeList] = new React.useState([]);
+  const [Happened, changeHappened] = new React.useState(false);
+  const history = useHistory();
 
-export default function SearchHistory(){
-    const [list, changeList] = new React.useState([]);
-    const [boolean, changeBoolean] = new React.useState(false);
-    const history = useHistory();
-    
+  function goToHistory() {
+    changeHappened(true);
+    Socket.on('received database info', (data) => {
+      changeList(data);
+    });
+  }
 
-    function goToHistory(){
-        changeBoolean(true);
-        console.log("Before waiting on socket.")
-        Socket.on("received database info", (data) => {
-            changeList(data);
-        });
-    }
-    
-    function goToSearchEngine(){
-        history.push("/content");
-    }
-    
-    if(boolean == false){
-        console.log("Before emit.")
-        Socket.emit("request search history", 1);
-        console.log("Before call to goToHistory")
-        goToHistory();
-    }
-    console.log("Going to display.")
-    var objects = list;
-    //console.log(objects)
-    if(objects.length != 0){
-        return <div>
+  function goToSearchEngine() {
+    history.push('/content');
+  }
+
+  if (Happened === false) {
+    Socket.emit('request search history');
+    goToHistory();
+  }
+  const objects = List;
+  if (objects.length !== 0) {
+    return (
+      <div>
         <h1>List of all searches</h1>
-        {   
+        {
             objects.map(
-                      (listing) => (
-                        <p>
-                          Address: {listing.address}
-                          <br />
-                          <img id="house" alt="" src={listing.home_image} />
-                          <br />
-                          Low Price: ${listing.price_low}
-                          <br />
-                          High Price: {listing.price_high}
-                          <br />
-                          Distance from Address: {listing.distance}
-                          <hr />
-                        </p>
-                      ),
-                    )
+              (listing) => (
+                <p>
+                  Address:
+                  {' '}
+                  {listing.address}
+                  <br />
+                  Low Price: $
+                  {listing.price_low}
+                  <br />
+                  High Price:
+                  {' '}
+                  {listing.price_high}
+                  <br />
+                  Distance from Address:
+                  {' '}
+                  {listing.distance}
+                  <hr />
+                </p>
+              ),
+            )
         }
         <button type="button" onClick={goToSearchEngine}>Go back to search page.</button>
-        </div>;
-    }
-    else {
-        return <div>
-        <h1>No searches for this user.</h1>
-        <button type="button" onClick={goToSearchEngine}>Go back to search page.</button>
-        </div>;
-    }
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>No searches for this user.</h1>
+      <button type="button" onClick={goToSearchEngine}>Go back to search page.</button>
+    </div>
+  );
 }
