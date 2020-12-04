fb9b59caf777a8ff59658e739bfe6ea6458c04bc
Neha Jagtap <ec2-user@ip-172-31-20-119.ec2.internal>
2020-12-04 23:27:30 +0000

    More styling for landing page

--- scripts/GuestGreeting.jsx
+++ scripts/GuestGreeting.jsx
@@ -7,7 +7,10 @@ import './GuestGreeting.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
-
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles({
   title: {
@@ -16,8 +19,25 @@ const useStyles = makeStyles({
     
   },
   header: {
-    fontSize: '4rem',
-    margin: "4.3rem"
+    fontSize: '3rem',
+    margin: "2rem"
+  },
+  box: {
+    minWidth: 275,
+    minHeight: 100,
+    backgroundColor: "#f2f2fc"
+  },
+  pos: {
+    fontSize:'2rem'
+  },
+  name: {
+    padding: 2,
+    minWidth: 200,
+    minHeight: 120,
+    backgroundColor: "#f2f2fc"
+  },
+  link: {
+    fontSize: '1.5rem'
   }
   }
 );
@@ -39,10 +59,78 @@ export default function GuestGreeting() {
         <Typography variant="h1" component="div" align="center" className={classes.header}>
         About
         </Typography>
+        <Card className={classes.box}>
+          <CardContent>
+          <Typography className={classes.pos} align="center">
+            Our website tailors your home search to your specific commute, providing information about the commute from each listing to your desired destination.
+          </Typography>
+          </CardContent>
+        </Card>
         <Divider />
         <Typography variant="h1" component="div" align="center" className={classes.header}>
         Technologies Used
         </Typography>
+        <Card className={classes.box}>
+          <CardContent>
+          <Typography className={classes.pos} align="center">
+            React, Python, PostgreSQL, MaterialUI, Heroku, Git, CircleCI, Rapid API (Realtor), Google Maps API, ESLint, PyLint
+          </Typography>
+          </CardContent>
+        </Card>
+        <Divider />
+        <Typography variant="h1" component="div" align="center" className={classes.header}>
+        Made By
+        </Typography>
+        <Grid container spacing={3}>
+        <Grid item xs={6} sm={3}>
+        <Card className={classes.name} align="center">
+          <CardContent>
+          <Typography className={classes.pos} align="center">
+            Ali Alkhateeb 
+          </Typography>
+          <Link className={classes.link} href="https://github.com/alialkhateeb99" target="_blank">
+            GitHub
+          </Link>
+          </CardContent>
+        </Card>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+        <Card className={classes.name} align="center">
+          <CardContent>
+          <Typography className={classes.pos} align="center">
+            Neha Jagtap
+          </Typography>
+          <Link className={classes.link} href="https://github.com/nehajgtp" target="_blank">
+            GitHub
+          </Link>
+          </CardContent>
+        </Card>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+        <Card className={classes.name} align="center">
+          <CardContent>
+          <Typography className={classes.pos} align="center">
+            Matthew Meeh
+          </Typography>
+          <Link className={classes.link} href="https://github.com/Matthew-J-M" target="_blank">
+            GitHub
+          </Link>
+          </CardContent>
+        </Card>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+        <Card className={classes.name} align="center">
+          <CardContent>
+          <Typography className={classes.pos} align="center">
+            Kevin Ng
+          </Typography>
+          <Link className={classes.link} href="https://github.com/kevinng250" target="_blank">
+            GitHub
+          </Link>
+          </CardContent>
+        </Card>
+        </Grid>
+        </Grid>
       </div>
     </div>
     </html>
