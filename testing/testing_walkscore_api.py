098e28e8c860eecae9631283228e43bd6d94f34c
Matthew-J-M <mjm227@njit.edu>
2020-12-08 05:27:06 +0000

    linted testing walkscore

--- testing/testing_walkscore_api.py
+++ testing/testing_walkscore_api.py
@@ -1,98 +1,170 @@
+'''
+This file tests walkscore_api
+'''
 from os.path import dirname, join
 import sys
-sys.path.append(join(dirname(__file__), "../"))
-
 import unittest
 import os
 import unittest.mock as mock
 from mock import patch, call
 import walkscore_api
 import requests
+sys.path.append(join(dirname(__file__), "../"))
+
+
 
 KEY_INPUT = "key_input"
 KEY_EXPECTED = "key_expected"
+
+
 class MockedResponse:
+    '''
+    Defines functions that get data
+    '''
     def __init__(self, json_data):
+        '''
+        creates the class
+        '''
         self.json_data = json_data
+
     def json(self):
+        '''
+        ...
+        '''
         return self.json_data
 
+
 class MockGetWalkscoreInfo(unittest.TestCase):
+    '''
+    Main Class
+    '''
     def setUp(self):
-        self.success_test_params ={
-            KEY_INPUT:{
-                "home_street":"",
-                "home_city":"",
-                "home_state_code":"",
-                "home_lon":"",
-                "home_lat":""
-                },
-            KEY_EXPECTED:{
-                "walkscore":"hello",
-                "description":"sup",
-                "logo":"something",
-                "more_info_link":"something",
-                "walkscore_link":"something else"
-                }
+        self.success_test_params = {
+            KEY_INPUT: {
+                "home_street": "",
+                "home_city": "",
+                "home_state_code": "",
+                "home_lon": "",
+                "home_lat": "",
+            },
+            KEY_EXPECTED: {
+                "walkscore": "hello",
+                "description": "sup",
+                "logo": "something",
+                "more_info_link": "something",
+                "walkscore_link": "something else",
+            },
         }
+
     def mock_request(self, url):
-        d ={
-            "walkscore":"hello",
-            "description":"sup",
-            "logo_url":"something",
-            "more_info_link":"something",
-            "ws_link":"something else"
+        '''
+        Mocks the request
+        '''
+        variables = {
+            "walkscore": "hello",
+            "description": "sup",
+            "logo_url": "something",
+            "more_info_link": "something",
+            "ws_link": "something else",
         }
-        r = MockedResponse(d)
-        return r
-        
+        response = MockedResponse(variables)
+        return response
+
     def test_get_walkscore_info(self):
+        '''
+        Does the tests
+        '''
         test_case = self.success_test_params
         with mock.patch("requests.get", self.mock_request):
-            inp = test_case[KEY_INPUT]
-            result = walkscore_api.get_walkscore_info(inp["home_street"], inp["home_city"], \
-                    inp["home_state_code"], inp["home_lon"], inp["home_lat"])
+            input_case = test_case[KEY_INPUT]
+            result = walkscore_api.get_walkscore_info(
+                input_case["home_street"],
+                input_case["home_city"],
+                input_case["home_state_code"],
+                input_case["home_lon"],
+                input_case["home_lat"],
+            )
             self.assertEqual(result, test_case[KEY_EXPECTED])
-            
-    def test_get_walkscore_info_HTTPErr(self):
+
+    def test_get_walkscore_info_http_err(self):
+        '''
+        Checks error handling for HTTP
+        '''
         test_case = self.success_test_params
-        with mock.patch("requests.get") as errMock:
-            errMock.side_effect = requests.exceptions.HTTPError
-            inp = test_case[KEY_INPUT]
-            result = walkscore_api.get_walkscore_info(inp["home_street"], inp["home_city"], \
-                    inp["home_state_code"], inp["home_lon"], inp["home_lat"])
-                    
-    def test_get_walkscore_info_ConnectionErr(self):
+        with mock.patch("requests.get") as err_mock:
+            err_mock.side_effect = requests.exceptions.HTTPError
+            input_case = test_case[KEY_INPUT]
+            result = walkscore_api.get_walkscore_info(
+                input_case["home_street"],
+                input_case["home_city"],
+                input_case["home_state_code"],
+                input_case["home_lon"],
+                input_case["home_lat"],
+            )
+
+    def test_get_walkscore_info_connection_err(self):
+        '''
+        Checks some kind of Connection Error handling
+        '''
         test_case = self.success_test_params
-        with mock.patch("requests.get") as errMock:
-            errMock.side_effect = requests.exceptions.ConnectionError
-            inp = test_case[KEY_INPUT]
-            result = walkscore_api.get_walkscore_info(inp["home_street"], inp["home_city"], \
-                    inp["home_state_code"], inp["home_lon"], inp["home_lat"])
-                    
-    def test_get_walkscore_info_TimeoutErr(self):
+        with mock.patch("requests.get") as err_mock:
+            err_mock.side_effect = requests.exceptions.ConnectionError
+            input_case = test_case[KEY_INPUT]
+            result = walkscore_api.get_walkscore_info(
+                input_case["home_street"],
+                input_case["home_city"],
+                input_case["home_state_code"],
+                input_case["home_lon"],
+                input_case["home_lat"],
+            )
+
+    def test_get_walkscore_info_timeout_err(self):
+        '''
+        Checks timeout error handling
+        '''
         test_case = self.success_test_params
-        with mock.patch("requests.get") as errMock:
-            errMock.side_effect = requests.exceptions.Timeout
-            inp = test_case[KEY_INPUT]
-            result = walkscore_api.get_walkscore_info(inp["home_street"], inp["home_city"], \
-                    inp["home_state_code"], inp["home_lon"], inp["home_lat"])
-                    
-    def test_get_walkscore_info_HRequestException(self):
+        with mock.patch("requests.get") as err_mock:
+            err_mock.side_effect = requests.exceptions.Timeout
+            input_case = test_case[KEY_INPUT]
+            result = walkscore_api.get_walkscore_info(
+                input_case["home_street"],
+                input_case["home_city"],
+                input_case["home_state_code"],
+                input_case["home_lon"],
+                input_case["home_lat"],
+            )
+
+    def test_get_walkscore_info_hrequest_exception(self):
+        '''
+        Checks a specific bad request handling
+        '''
         test_case = self.success_test_params
-        with mock.patch("requests.get") as errMock:
-            errMock.side_effect = requests.exceptions.RequestException
-            inp = test_case[KEY_INPUT]
-            result = walkscore_api.get_walkscore_info(inp["home_street"], inp["home_city"], \
-                    inp["home_state_code"], inp["home_lon"], inp["home_lat"])
-                    
-    def test_get_walkscore_info_IndexError(self):
+        with mock.patch("requests.get") as err_mock:
+            err_mock.side_effect = requests.exceptions.RequestException
+            input_case = test_case[KEY_INPUT]
+            result = walkscore_api.get_walkscore_info(
+                input_case["home_street"],
+                input_case["home_city"],
+                input_case["home_state_code"],
+                input_case["home_lon"],
+                input_case["home_lat"],
+            )
+
+    def test_get_walkscore_info_index_err(self):
+        '''
+        Check index error handling
+        '''
         test_case = self.success_test_params
-        with mock.patch("requests.get") as errMock:
-            errMock.side_effect = IndexError
-            inp = test_case[KEY_INPUT]
-            result = walkscore_api.get_walkscore_info(inp["home_street"], inp["home_city"], \
-                    inp["home_state_code"], inp["home_lon"], inp["home_lat"])
-    
+        with mock.patch("requests.get") as err_mock:
+            err_mock.side_effect = IndexError
+            input_case = test_case[KEY_INPUT]
+            result = walkscore_api.get_walkscore_info(
+                input_case["home_street"],
+                input_case["home_city"],
+                input_case["home_state_code"],
+                input_case["home_lon"],
+                input_case["home_lat"],
+            )
+
 if __name__ == "__main__":
     unittest.main()
